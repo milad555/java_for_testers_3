e65b964f306783a5f5b01baca45dbf992369ff5b
Milad <gebriel007@gmail.com>
2024-06-12 19:02:13 -0700

    lesson object

--- sandbox/src/main/java/ru/stqa/geometry/Hello.java
+++ sandbox/src/main/java/ru/stqa/geometry/Hello.java
@@ -1,4 +1,12 @@
 package ru.stqa.geometry;
+import java.io.File;
 
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        var configFile = new File("sandbox/build.gradle");
+        System.out.println(configFile.getAbsoluteFile());
+        System.out.println(configFile.exists());
+    }
 }
--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -5,11 +5,15 @@ public class Square {
         System.out.println(text(side));
     }
 
-    private static String text(double side) {
-        return String.format("Площадь квадрата со стороной %f = %f",  side,  squareArea(side));
+    public static String text(double side) {
+        return String.format("Площадь квадрата со стороной %f = %f",  side,  area(side));
     }
 
-    private static double squareArea(double a) {
+    public static double area(double a) {
         return a * a;
+    }
+
+    public static double perimeter(double a) {
+        return 4*a;
     }
 }
